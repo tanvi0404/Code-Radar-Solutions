c422615e0dbadf15c4dfe97de5fd06463c075452
tanvi0404 <thakurtanvi253@gmail.com>
2025-01-26 14:03:13 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int tanvi;
+    scanf("%d",&tanvi);
+    if (tanvi >=90)
+    printf("A");
+    else if (tanvi >= 80 && tanvi < 90)
+    printf("B");
+    else if (tanvi >= 70 && tanvi <80)
+    printf("C");
+    else if(tanvi >= 60 && tanvi < 70)
+    printf("D");
+    else 
+    printf("F")
+    return 0;
+}
