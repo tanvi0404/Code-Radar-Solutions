e600d4a73b5a3f959f073a1ede2547ccf0578975
tanvi0404 <thakurtanvi253@gmail.com>
2025-01-26 13:52:07 +0530

    Solution to Find Greater Number

---
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int tanvi,thakur;
+    scanf("%d %d",&tanvi,&thakur);
+    if (tanvi >= thakur)
+    printf("%d",tanvi);
+    else
+    printf("%d",thakur);
+    return 0;
+}
