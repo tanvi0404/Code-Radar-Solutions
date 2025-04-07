2003bfeacab165a6b1e2b9417537fec59b4f02f5
tanvi0404 <thakurtanvi253@gmail.com>
2025-04-07 09:59:36 +0530

    Solution to Find the Smallest Missing Positive Integer

---
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -0,0 +1,18 @@
+// Your code here...
+#include <stdio.h>
+int findKthMissing(int arr[],int size,int k){
+    int count=0,num=1,index=0;
+    while(count<k){
+        if(index<size && arr[index]==num){
+            index++;
+        }
+        else{
+            count++;
+            if(count==k){
+                return num;
+            }
+        }
+        num++;
+    }
+    return -1;
+}
